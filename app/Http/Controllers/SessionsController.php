38827dbff979d229280c1031933a5fcabc5a9726
taowendi <twd121@126.com>
2018-11-08 15:17:35 +0800

    完成了用户编辑，修改，删除等动作

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,11 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
     public function create(){
         return view('sessions.create');
     }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,20 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',[
+            'except'=>['show','create','store','index']
+        ]);
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
+    public function index(){
+            $users=User::paginate(10);
+            return view('users.index',compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
@@ -39,5 +53,38 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程');
         return redirect()->route('users.show',[$user]);
+    }
+
+    /**
+     * @Notes:修改用户
+     * @param User $user
+     * @author:taowendi
+     * @Time:2018-11-08 11:06
+     */
+    public function edit(User $user){
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+        $this->authorize('update', $user);
+        $data=[];
+        $data['name']=$request->name;
+        if($request->password){
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show',$user->id);
+    }
+
+    public function destroy(User $user){
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
     }
 }
